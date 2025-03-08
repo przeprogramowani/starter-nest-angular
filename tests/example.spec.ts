531facd14283d58b665fbc7ac76949bad1184a6d
“mkczarkowski” <marcinczarkowski@onet.eu>
2025-03-08 19:52:42 +0100

    chore: add smoke test

---
+++ tests/example.spec.ts
@@ -0,0 +1,7 @@
+import {expect, test} from "@playwright/test";
+
+test("smoke test", async ({page}) => {
+  await page.goto("http://localhost:4200/");
+
+  await expect(page.getByText("Flashcards")).toBeVisible();
+});
