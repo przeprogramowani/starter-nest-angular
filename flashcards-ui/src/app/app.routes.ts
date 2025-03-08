bf74b3817f5981e4b33da7aef29949367af1ad5e
“mkczarkowski” <marcinczarkowski@onet.eu>
2025-03-08 10:45:58 +0100

    feat: delete unused component

--- flashcards-ui/src/app/app.routes.ts
+++ flashcards-ui/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { FlashcardsCreateComponent } from './flashcards-create/flashcards-create.component';
-import { FlashcardsGenerateComponent } from './flashcards-generate/flashcards-generate.component';
+
 import { FlashcardsListComponent } from './flashcards-list/flashcards-list.component';
 
 export const routes: Routes = [
